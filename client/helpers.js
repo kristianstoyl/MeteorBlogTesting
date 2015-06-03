4b6a1c5e0fe9e03022ff76749d8aafe86a4d2d7a
neek123 <kristian.stoylen93@gmail.com>
2015-06-03 20:25:04 +0200

    finished leaderboard

--- client/helpers.js
+++ client/helpers.js
@@ -1,15 +1,11 @@
 Session.set('counter', 0);
-Session.set('name', '');
 
 Template.home.helpers({
     counter: function () {
-        //return Counter.find().fetch()[0]._id.count;
         return Session.get('counter');
     },
     points: function () {
-        //return Meteor.user(); //thyis was just to see what Meteor.user returned
         return Session.get('counter') * 4;
-        //return Counter.find().count() * 4;
     }
 });
 
@@ -20,10 +16,12 @@ Template.leaderboard.helpers({
 });
 Template.player.helpers({
     userName: function () {
-        //console.log(this.user);
         var result = Meteor.users.findOne({_id: this.user});
-        if(typeof result.profile.name !== undefined)
-            return result.profile.name;
+        if(result === undefined) {
+        } else {
+            if (typeof result.profile.name !== undefined)
+                return result.profile.name;
+        }
     }
 });
 
